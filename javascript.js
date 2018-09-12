f487257a1107d3465fa7cd3fbefe36db9f4d0c86
Kate2106 <39519608+Kate2106@users.noreply.github.com>
2018-09-12 11:20:41 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+﻿function f()
+{
+alert("HELLO!!!")
+document.fgColor="blue";
+document.bgColor="pink";
+}
+function f1()
+{
+document.fgColor="black";
+}
+function f2()
+{
+document.bgColor="yellow";
+}
+function f3()
+{
+alert("Нам было сложно")
+}
