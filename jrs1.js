2d68f8ca1e487ac2c00b21b85ceceb586a562a85
Kate2106 <39519608+Kate2106@users.noreply.github.com>
2018-12-05 12:55:33 +0200

    Update jrs1.js

--- jrs1.js
+++ jrs1.js
@@ -12,15 +12,26 @@ return elements; }
 function f1(input)
 {		
 		var x1="123456789-";
-	var a = input.indexOf("--");
-	if(input == "" )
+	if(input.indexOf("")==-1)
 {
 	return "Fail";
 }
-	if(input == "--")
+	if(input.indexOf("--")==-1)
 {
 	return "Fail";
 }
-else
-{return "Okay"}	
+	if(input.indexOf("-")==-1)
+{
+	return "Fail";
+}
+	if(input.lastIndexOf("-")==-1)
+{
+	return "Fail";
+}
+for(i=0;i<input.length;i++)
+{	
+	if(x1.indexOf(input.charAt(i))==-1)
+		return "Fail"
+}
+return "Okay"
 }
