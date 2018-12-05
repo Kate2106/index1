d0a11e1d3290602243f3243fa673f691c04c08a1
Kate2106 <39519608+Kate2106@users.noreply.github.com>
2018-12-05 13:39:15 +0200

    Update jrs1.js

--- jrs1.js
+++ jrs1.js
@@ -11,27 +11,22 @@ elements.push(element);
 return elements; }
 function f1(input)
 {		
-		var x1="123456789-";
-	if(input.indexOf("")==-1)
+	var x1=["1","2","3","4","5","6","7","8","9","-"];
+if(input.indexOf("")==0)
 {
 	return "Fail";
 }
-	if(input.indexOf("--")==-1)
+	var x2 = new String(document.getElementsById("num"));
+	var x3 = x2.split("-");
+if(x3.indexOf("")==0)
 {
 	return "Fail";
 }
-	if(input.indexOf("-")==0)
-{
-	return "Fail";
-}
-	if(input.lastIndexOf("-")==input.length)
-{
-	return "Fail";
-}
+	
 for(i=0;i<input.length;i++)
 {	
 	if(x1.indexOf(input.charAt(i))==-1)
-		return "Fail"
+	return "Fail";
 }
-return "Okay"
+return "Okay";
 }
